a4be4d0e136e0996b482e5fca619d42931dd7020
Chrismario123 <chrismariolipehunim@gmail.com>
2023-10-31 17:55:57 +0100

    task 101

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,13 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 
-remove_char_at = __import__('101-remove_char_at').remove_char_at
+def remove_char_at(str, n):
 
-print(remove_char_at("Best School", 3))
+    if n < 0:
 
-print(remove_char_at("Chicago", 2))
+        return str
 
-print(remove_char_at("C is fun!", 0))
+    else:
 
-print(remove_char_at("School", 10))
+        str = str[0:n] + str[n+1:]
 
-print(remove_char_at("Python", -2))
+    return str
