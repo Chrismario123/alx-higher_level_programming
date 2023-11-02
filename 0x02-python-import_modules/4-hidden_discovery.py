513cf167f16a64d3c998ad4fc9ac16866ffb0070
Chrismario123 <chrismariolipehunim@gmail.com>
2023-11-02 17:45:32 +0100

    task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 import hidden_4
 
+
 def print_module_names():
     module_names = dir(hidden_4)
     fn = [n for n in module_names if not n.startswith('__')]
     fn.sort()
     for name in fn:
         print(name)
+
 
 if _name_ == '_main_':
     print_module_names()
