13af4c152866bb1fbda85c8c5a8559a745f09608
Chrismario123 <chrismariolipehunim@gmail.com>
2023-11-02 18:42:48 +0100

    task 3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,13 +1,10 @@
 #!/usr/bin/python3
-
 if __name__ == "__main__":
-
     import sys
-
-    result = 0
-
-    for i in sys.argv:
-
-        result += int(i)
-
-        print("{}".format(result))
+    res = 0
+    if len(sys.argv) == 1:
+        print("{}".format(int(0)))
+    else:
+        for i in range(1, len(sys.argv)):
+            res = res + int(sys.argv[i])
+        print(f"{res}")
